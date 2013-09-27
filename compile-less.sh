77d9daf49ad4dbc2e515f394c9c7f52ed4b4f814
Hugo Roy <hugo@fsfe.org>
2013-09-27 14:45:39 +0200

    bootstrap and less compilation

---
+++ compile-less.sh
@@ -0,0 +1,6 @@
+lessc local.less -x local.css \
+    && lessc hroy.less -x hroy.css \
+    && lessc tweets/local.less -x tweets/local.css \
+    && lessc posts/local.less -x posts/local.css \
+    && lessc images/local.less -x images/local.css
+
