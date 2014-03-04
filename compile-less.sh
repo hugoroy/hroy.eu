c6d104d9252279736bc641e064dd1a1f04045d3b
Hugo Roy <hugo@fsfe.org>
2014-03-04 17:15:13 +0100

    new local files

--- compile-less.sh
+++ compile-less.sh
@@ -1,6 +1,10 @@
 lessc local.less -x local.css \
     && lessc hroy.less -x hroy.css \
     && lessc tweets/local.less -x tweets/local.css \
+    && lessc timeline/local.less -x timeline/local.css \
     && lessc posts/local.less -x posts/local.css \
+    && lessc tips/local.less -x tips/local.css \
+    && lessc notes/local.less -x notes/local.css \
+    && lessc configs/local.less -x conigs/local.css \
     && lessc images/local.less -x images/local.css
 
