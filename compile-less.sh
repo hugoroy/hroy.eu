9ce7a3c7abbd549590eecf3483d95b914824c7cc
Hugo Roy <hugo@fsfe.org>
2014-03-04 17:17:23 +0100

    remove local for timeline

--- compile-less.sh
+++ compile-less.sh
@@ -1,7 +1,6 @@
 lessc local.less -x local.css \
     && lessc hroy.less -x hroy.css \
     && lessc tweets/local.less -x tweets/local.css \
-    && lessc timeline/local.less -x timeline/local.css \
     && lessc posts/local.less -x posts/local.css \
     && lessc tips/local.less -x tips/local.css \
     && lessc notes/local.less -x notes/local.css \
