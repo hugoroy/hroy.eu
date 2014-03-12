5f8c8d976316b4bdcc0e06770b86fbf04fc90624
Hugo Roy <hugo@fsfe.org>
2014-03-12 17:52:25 +0100

    typo config style

--- compile-less.sh
+++ compile-less.sh
@@ -4,6 +4,6 @@ lessc local.less -x local.css \
     && lessc posts/local.less -x posts/local.css \
     && lessc tips/local.less -x tips/local.css \
     && lessc notes/local.less -x notes/local.css \
-    && lessc configs/local.less -x conigs/local.css \
+    && lessc configs/local.less -x configs/local.css \
     && lessc images/local.less -x images/local.css
 
