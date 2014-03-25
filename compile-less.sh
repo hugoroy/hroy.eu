c0d3dad0c9347c7fbe6d2c745b0cb5deed56be40
Hugo Roy <hugo@fsfe.org>
2014-03-25 17:55:54 +0100

    timeline local stylesheet

--- compile-less.sh
+++ compile-less.sh
@@ -5,5 +5,6 @@ lessc local.less -x local.css \
     && lessc tips/local.less -x tips/local.css \
     && lessc notes/local.less -x notes/local.css \
     && lessc configs/local.less -x configs/local.css \
-    && lessc images/local.less -x images/local.css
+    && lessc images/local.less -x images/local.css \
+    && lessc timeline/local.less -x timeline/local.css
 
