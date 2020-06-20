1a13ad18207a7cc5b288e06aa210eb495ed6bb44
Hugo Roy <hugo@fsfe.org>
2020-06-20 14:40:04 +0200

    Rebuilding the website a bit from scratch

--- talks/CNNumStrasbourg2015-01-09/build.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-pandoc PitchHugoRoyToSDR.md -o PitchHugoRoyToSDR.md.revealjs.html --variable lang=fr --standalone --smart -t revealjs --email-obfuscation=references --variable theme=night
